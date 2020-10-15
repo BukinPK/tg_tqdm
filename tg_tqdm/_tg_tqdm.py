e5a0b36f8955f671a5795c11283570dc5d07c763
Roman Troyan <bukinpk@gmail.com>
2020-10-15 14:14:19 +0300

    Update _tg_tqdm.py

--- tg_tqdm/_tg_tqdm.py
+++ tg_tqdm/_tg_tqdm.py
@@ -1,30 +1,32 @@
 import telepot
+import telegram.ext
 from tqdm import tqdm
 from datetime import datetime
 
 
 class _TelegramIO():
-    def __init__(self, token, chat_id, show_last_update=True):
-        self.bot = telepot.Bot(token)
+    def __init__(self, token, chat_id, show_last_update=True, parse_mode=None):
+        self.updater = telegram.ext.Updater(token=token, use_context=True,
+                                            defaults=telegram.ext.Defaults(parse_mode=parse_mode))
         self.chat_id = chat_id
-        self.text = self.prev_text = '<< Init tg_tqdm bar >>'
-        self.message_id = self.bot.sendMessage(chat_id, self.text)['message_id']
+        self.parse_mode = parse_mode
+        self.text = self.prev_text = '[init tg_tqdm bar]'
+        self.message_id = self.updater.bot.send_message(chat_id, self.text)['message_id']
         self.show_last_update = show_last_update
 
     def write(self, s):
         new_text = s.strip().replace('\r', '')
-        if len(new_text) != 0:
+        if new_text:
             self.text = new_text
 
     def flush(self):
         if self.prev_text != self.text:
-            if '%' in self.text:
-                self.bot.editMessageText((self.chat_id, self.message_id), self.text +
-                                         '\nLast update: {}'.format(datetime.now()) if self.show_last_update else self.text)
-                self.prev_text = self.text
+            text = self.text + '\nLast update: {datetime.now()}' if self.show_last_update else self.text
+            self.updater.bot.edit_message_text(text, self.chat_id, self.message_id)
+            self.prev_text = self.text
 
 
-def tg_tqdm(iterable, token, chat_id, show_last_update=True,
+def tg_tqdm(iterable, token, chat_id, show_last_update=True, parse_mode=None,
             desc=None, total=None, leave=True, ncols=None, mininterval=1.0, maxinterval=10.0,
             miniters=None, ascii=False, disable=False, unit='it',
             unit_scale=False, dynamic_ncols=False, smoothing=0.3,
@@ -53,26 +55,27 @@ def tg_tqdm(iterable, token, chat_id, show_last_update=True,
             Like in tqdm
             
     """
-    tg_io = _TelegramIO(token, chat_id, show_last_update)
-    return tqdm(iterable=iterable,
-                desc=desc,
-                total=total,
-                leave=leave,
-                file=tg_io,
-                ncols=ncols,
-                mininterval=mininterval,
-                maxinterval=maxinterval,
-                miniters=miniters,
-                ascii=ascii,
-                disable=disable,
-                unit=unit,
-                unit_scale=unit_scale,
-                dynamic_ncols=dynamic_ncols,
-                smoothing=smoothing,
-                bar_format=bar_format,
-                initial=initial,
-                position=position,
-                postfix=postfix,
-                unit_divisor=unit_divisor,
-                gui=gui,
-                **kwargs)
+    tg_io = _TelegramIO(token, chat_id, show_last_update, parse_mode)
+    return tqdm(
+        iterable=iterable,
+        desc=desc,
+        total=total,
+        leave=leave,
+        file=tg_io,
+        ncols=ncols,
+        mininterval=mininterval,
+        maxinterval=maxinterval,
+        miniters=miniters,
+        ascii=ascii,
+        disable=disable,
+        unit=unit,
+        unit_scale=unit_scale,
+        dynamic_ncols=dynamic_ncols,
+        smoothing=smoothing,
+        bar_format=bar_format,
+        initial=initial,
+        position=position,
+        postfix=postfix,
+        unit_divisor=unit_divisor,
+        gui=gui,
+        **kwargs)
